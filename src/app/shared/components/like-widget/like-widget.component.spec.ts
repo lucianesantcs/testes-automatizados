2edb3b8a0e5a07a9a2e4a2fa49c004adc703da8d
Luciane Santos <lucianesantcs@gmail.com>
2021-10-17 19:29:13 -0300

    feat(testes-automatizados): aula final 5

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from "@angular/core/testing";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 import { LikeWidgetComponent } from "./like-widget.component";
 import { LikeWidgetModule } from "./like-widget.module";
 
@@ -8,14 +6,10 @@ describe(LikeWidgetComponent.name, () => {
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
     let component: LikeWidgetComponent = null;
 
-    // usa-se async await e complieComponents para garantia caso builder usado para carregar o modulo mude
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            //providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }] // alternativa não recomendada para fixture.detectChanges();
             imports: [LikeWidgetModule],
-            // imports: [FontAwesomeModule],
-            // declarations: [LikeWidgetComponent],
-            // providers: [UniqueIdService]
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponent);
@@ -23,7 +17,6 @@ describe(LikeWidgetComponent.name, () => {
     });
 
     it('Should create component', () => {
-        //const component = fixture.componentInstance; // instância do componente LikeWidgetComponent
         expect(component).toBeTruthy();
     });
 
@@ -34,23 +27,11 @@ describe(LikeWidgetComponent.name, () => {
     // });
 
     it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
-        //const component = fixture.componentInstance;
         const someId = 'someId';
-        fixture.detectChanges(); // chama métodos de ciclo de vida do angular
+        fixture.detectChanges();
         component.id = someId;
         expect(component.id).toBeTruthy();
     });
-
-    // teste de @Output com done(), mas melhor usar método spyOn
-        // it(`${LikeWidgetComponent.prototype.like.name} 
-        //  should trigger (@Output liked) when called`, done => {
-        //         fixture.detectChanges();
-        //         component.liked.subscribe(() => { // subscribe pois o EventEmitter é um observable
-        //             expect(true).toBeTrue(); // apenas verifica se foi chamado
-        //             done(); // chama para função assíncrona e permite que o teste mostre que o teste falhou 
-        //         });
-        //         component.like();
-        //     });
 
     // teste de @Output, melhor maneira para EventEmitter
     it(`${LikeWidgetComponent.prototype.like.name} 
