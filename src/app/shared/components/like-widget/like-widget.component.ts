8bdf281a44f5dceff2c62a0a9f44b192a7538729
Luciane Santos <lucianesantcs@gmail.com>
2021-10-17 11:37:29 -0300

    feat(testes-automatizados): aula 3

---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss']
+})
+export class LikeWidgetComponent implements OnInit {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id = null;
+
+  public faIcon = { faThumbsUp };
+
+  constructor(private uniqueIdService: UniqueIdService) { }
+
+  public like(): void {
+    this.liked.emit();
+  }
+
+  ngOnInit(): void {
+    if(!this.id) {
+      this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
+    }
+  }
+
+}
---
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LikeWidgetComponent } from './like-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+
+@NgModule({
+  declarations: [LikeWidgetComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [LikeWidgetComponent],
+  providers: [UniqueIdService]
+})
+export class LikeWidgetModule {}
